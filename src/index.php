8f752cae35e1f34fc77c695105ad0b42a224ec83
zlatan-at-584427966635 <zlatan@784f43849fca.ant.amazon.com>
2017-12-15 06:18:51 -0800

    full form, no image#

--- src/index.php
+++ src/index.php
@@ -47,6 +47,20 @@
             width: 307px;
             height: 23px;
         }
+        .auto-style10 {
+            text-align: right;
+        }
+        .auto-style11 {
+            text-align: right;
+            height: 23px;
+        }
+        .auto-style12 {
+            width: 383px;
+            height: 69px;
+        }
+        .auto-style13 {
+            width: 81%;
+        }
     </style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
@@ -54,80 +68,102 @@
 
     <body>
         
-        <table style="width:100%;">
-        <tr>
-            <td colspan="3"><strong>Select Items to Purchase</strong></td>
-        </tr>
-        <tr>
-            <td class="auto-style1"><strong>Name</strong>: Jim Vandragt</td>
-            <td><strong>Manager</strong>: John McLaughin</td>
-            <td>&nbsp;</td>
-        </tr>
-        <tr>
-            <td class="auto-style1"><strong>Department</strong>: US9506-ProductDev</td>
-            <td><strong>Mail Stop:</strong> 0161</td>
-            <td>&nbsp;</td>
-        </tr>
-    </table>
-    <table style="width:100%;">
-        <tr>
-            <td class="auto-style3"><b>Site:</b></td>
-            <td class="auto-style5"><b>Tax Rate:</b></td>
-            <td class="auto-style4"><strong>Created On:</strong></td>
-        </tr>
-        <tr>
-            <td class="auto-style2">
-                <select id="Select1" name="D1" title="Design Yard">
-                    <option>Design Yard</option>
-                </select></td>
-            <td class="auto-style6">0.06</td>
-            <td>11/21/2017 8:28:00 AM</td>
-        </tr>
-        <tr>
-            <td class="auto-style2"><strong>Concierge:</strong></td>
-            <td class="auto-style6">&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr>
-        <tr>
-            <td class="auto-style2">
-                <select id="Select2" name="D2" title="Design Yard">
-                    <option>Bumstead, Candi</option>
-                </select></td>
-            <td class="auto-style6">&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr>
-    </table>
+    <table class="auto-style13">
+    <tr>
+        <td colspan="3"><strong>Select Items to Purchase</strong></td>
+    </tr>
+    <tr>
+        <td class="auto-style1"><strong>Name</strong>: Jim Vandragt</td>
+        <td><strong>Manager</strong>: John McLaughin</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td class="auto-style1"><strong>Department</strong>: US9506-ProductDev</td>
+        <td><strong>Mail Stop:</strong> 0161</td>
+        <td>&nbsp;</td>
+    </tr>
+</table>
+<table class="auto-style13">
+    <tr>
+        <td class="auto-style3"><b>Site:</b></td>
+        <td class="auto-style5"><b>Tax Rate:</b></td>
+        <td class="auto-style4"><strong>Created On:</strong></td>
+    </tr>
+    <tr>
+        <td class="auto-style2">
+            <select id="Select1" name="D1" title="Design Yard">
+                <option>Design Yard</option>
+            </select></td>
+        <td class="auto-style6">0.06</td>
+        <td>11/21/2017 8:28:00 AM</td>
+    </tr>
+    <tr>
+        <td class="auto-style2"><strong>Concierge:</strong></td>
+        <td class="auto-style6">&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td class="auto-style2">
+            <select id="Select2" name="D2" title="Design Yard">
+                <option>Bumstead, Candi</option>
+            </select></td>
+        <td class="auto-style6">&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+</table>
 
-    <table style="width:100%;">
-        <tr>
-            <td class="auto-style9"><strong>Item:</strong></td>
-            <td class="auto-style4"><strong>Price:</strong></td>
-            <td class="auto-style4"><strong>Qty:</strong></td>
-            <td class="auto-style4"><strong>Total:</strong></td>
-            <td class="auto-style4"><strong>Amenities:</strong></td>
-            <td class="auto-style4"><strong>Services:</strong></td>
-        </tr>
-        <tr>
-            <td class="auto-style8">
-                <select id="Select3" name="D3" title="Design Yard" class="auto-style7">
-                    <option>Stamps</option>
-                </select></td>
-            <td>$ 0.49</td>
-            <td>
-                <select id="Select4" name="D4">
-                    <option>1</option>
-                </select></td>
-            <td>
-                $ 0.49</td>
-            <td>$ 0.00</td>
-            <td>$ 0.49</td>
-        </tr>
-        <tr>
-            <td colspan="3">&nbsp;</td>
-            <td>&nbsp;</td>
-            <td colspan="2">&nbsp;</td>
-        </tr>
-    </table>
-    </body>
+<table class="auto-style13">
+    <tr>
+        <td class="auto-style9"><strong>Item:</strong></td>
+        <td class="auto-style4"><strong>Price:</strong></td>
+        <td class="auto-style4"><strong>Qty:</strong></td>
+        <td class="auto-style4"><strong>Total:</strong></td>
+        <td class="auto-style4"><strong>Amenities:</strong></td>
+        <td class="auto-style4"><strong>Services:</strong></td>
+    </tr>
+    <tr>
+        <td class="auto-style8">
+            <select id="Select3" name="D3" title="Design Yard" class="auto-style7">
+                <option>Stamps</option>
+            </select></td>
+        <td>$ 0.49</td>
+        <td>
+            <select id="Select4" name="D4">
+                <option>1</option>
+            </select></td>
+        <td>
+            $ 0.49</td>
+        <td>$ 0.00</td>
+        <td>$ 0.49</td>
+    </tr>
+    <tr>
+        <td colspan="3">FedEx Shipping($)&nbsp;
+            <input id="Text1" type="text" /></td>
+        <td>&nbsp;</td>
+        <td colspan="2">&nbsp;</td>
+    </tr>
+</table>
+
+<table class="auto-style13">
+    <tr>
+        <td class="auto-style10"><strong>Amenities:</strong></td>
+        <td>$ 0.00</td>
+        <td><strong>Comments:</strong></td>
+    </tr>
+    <tr>
+        <td class="auto-style10"><strong>Services (including FedEx):</strong></td>
+        <td>$ 0.49</td>
+        <td rowspan="3">
+            <textarea id="TextArea1" class="auto-style12" name="S1"></textarea></td>
+    </tr>
+    <tr>
+        <td class="auto-style11"><strong>Tax:</strong></td>
+        <td class="auto-style4">$ 0.00</td>
+    </tr>
+    <tr>
+        <td class="auto-style10"><strong>Total Cost:</strong></td>
+        <td>$ 0.49</td>
+    </tr>
+</table>
 
 </html>
