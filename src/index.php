649715ad0fb6a7561ca354173eaacbcdc91eca76
zlatan-at-584427966635 <zlatan@784f43849fca.ant.amazon.com>
2017-12-15 06:08:42 -0800

    try new

--- src/index.php
+++ src/index.php
@@ -48,29 +48,12 @@
             height: 23px;
         }
     </style>
-        <style>body {margin-top: 40px; background-color: #333;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
 
     <body>
-        <div class="container">
-            <div class="hero-unit">
-                <h1>Simple .NET App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
-                        echo fread($myfile,filesize("/var/www/my-vol/date"));
-                        fclose($myfile);
-                ?>
-
-            </div>
-        </div>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
+        
         <table style="width:100%;">
         <tr>
             <td colspan="3"><strong>Select Items to Purchase</strong></td>
